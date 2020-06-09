b7f7ee1362b743e63d509060a7818ef9487fb970
Takafumi Ochiai <taka@Takafumis-MacBook-Pro.local>
2020-06-09 21:06:09 +0900

    Modify messege

--- c++/2_D_circle_in_a_rectangle.cpp
+++ c++/2_D_circle_in_a_rectangle.cpp
@@ -6,8 +6,8 @@ int main(){
     int w,h,x,y,r;
     cin >> w >> h >> x >> y >> r;
     if(x+r <= w && x-r >= 0 && y+r <= h && y-r >= 0 ){
-        printf("yes\n");
+        printf("Yes\n");
     } else {
-        printf("no\n");
+        printf("No\n");
     }
 }
